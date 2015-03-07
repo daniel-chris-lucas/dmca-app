1aaec6f34ebe80c7cf65e0de04a1287ee7e66627
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-03-07 20:27:32 +0100

    Hooking into Mandrill

--- app/Http/Controllers/NoticesController.php
+++ app/Http/Controllers/NoticesController.php
@@ -19,6 +19,8 @@ class NoticesController extends Controller {
     public function __construct()
     {
         $this->middleware('auth');
+
+        parent::__construct();
     }
 
     /**
@@ -28,7 +30,7 @@ class NoticesController extends Controller {
      */
     public function index()
     {
-        return Auth::user()->notices;
+        return $this->user->notices;
     }
 
     /**
@@ -47,12 +49,11 @@ class NoticesController extends Controller {
      * Ask the user to confirm the DMCA that will be delivered.
      *
      * @param PrepareNoticeRequest $request
-     * @param Guard $auth
      * @return \Response
      */
-    public function confirm(PrepareNoticeRequest $request, Guard $auth)
+    public function confirm(PrepareNoticeRequest $request)
     {
-        $template = $this->compileDmcaTemplate($data = $request->all(), $auth);
+        $template = $this->compileDmcaTemplate($data = $request->all());
 
         session()->flash('dmca', $data);
 
@@ -82,14 +83,13 @@ class NoticesController extends Controller {
      * Compile the DMCA template from the form data.
      *
      * @param $data
-     * @param Guard $auth
      * @return mixed
      */
-    public function compileDmcaTemplate($data, Guard $auth)
+    public function compileDmcaTemplate($data)
     {
         $data = $data + [
-            'name'  => $auth->user()->name,
-            'email' => $auth->user()->email
+            'name'  => $this->user->name,
+            'email' => $this->user->email
         ];
 
         return view()->file(app_path('Http/Templates/dmca.blade.php'), $data);
@@ -99,7 +99,7 @@ class NoticesController extends Controller {
      * Create and persist a new DMCA notice.
      *
      * @param Request $request
-     * @return $this
+     * @return \Illuminate\Database\Eloquent\Model
      */
     private function createNotice(Request $request)
     {
@@ -107,7 +107,7 @@ class NoticesController extends Controller {
 
         $notice = Notice::open($data)->useTemplate($request->input('template'));
 
-        $notice = Auth::user()->notices()->save($notice);
+        $notice = $this->user->notices()->save($notice);
 
         return $notice;
     }
